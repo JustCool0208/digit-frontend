5a67ca6d6aa96cff017629b538696aad5873c1b7
Rohith <rohith02aug@gmail.com>
2025-03-07 23:35:29 +0530

    Initial commit

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ async function sendData() {
     const imageBase64 = canvas.toDataURL();
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://digit-backend-ax66.onrender.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ image: imageBase64, operation: operation, second_number: parseInt(secondNumber) })
